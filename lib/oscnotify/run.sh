21f3d3c52043ef387661c697b6ea2be3d4e18c11
Zack Scholl <zack.scholl@gmail.com>
2022-12-14 08:03:19 -0800

    update notify
    Former-commit-id: f5cf33981da63562137ae1dcb70b94c37d6709d8

--- lib/oscnotify/run.sh
+++ lib/oscnotify/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ps -ef | grep 'oscnotify' | grep -v grep | grep -v run | awk '{print $2}' | xargs -r kill -9
 cd "$(dirname "$0")"
-nohup ./oscnotify -path /home/we/dust/code/amenbreak/ >/dev/null 2>&1 &
+nohup ./oscnotify -ignore /home/we/dust/code/amenbreak/data -path /home/we/dust/code/amenbreak/ >/dev/null 2>&1 &

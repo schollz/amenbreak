cbd90d05d2e3b93fd82ac8589ed34643c5afaca7
Zack Scholl <zack.scholl@gmail.com>
2022-12-17 07:19:53 -0800

    add verbose

--- lib/install.sh
+++ lib/install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 cd /home/we/dust/audio
+echo "downloading amens"
 wget -q https://github.com/schollz/amenbreak/releases/download/audio2/amenbreak.tar
+echo "unzipping amens"
 tar -xvf amenbreak.tar
 rm amenbreak.tar
 cd /home/we/dust/data
+echo "downloading image data"
 wget -q https://github.com/schollz/amenbreak/releases/download/audio2/amenbreak_data.tar.gz
+echo "unzipping image data"
 tar -xvzf amenbreak_data.tar.gz
 rm amenbreak_data.tar.gz

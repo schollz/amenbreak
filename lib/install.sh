4241b1a0ce3314fb5b7aa5ea084294a19fa9e7fd
Zack Scholl <zack.scholl@gmail.com>
2022-12-16 22:16:38 -0800

    fix installer

--- lib/install.sh
+++ lib/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd /home/we/dust/audio
-wget https://github.com/schollz/amenbreak/releases/download/assets/amenbreak.tar.gz
+wget https://github.com/schollz/amenbreak/releases/download/audio2/amenbreak.tar.gz
 tar -xvzf amenbreak.tar.gz
 rm amenbreak.tar.gz
 cd /home/we/dust/data
-wget https://github.com/schollz/amenbreak/releases/download/data/amenbreak.tar.gz
-tar -xvzf amenbreak.tar.gz
-rm amenbreak.tar.gz
+wget https://github.com/schollz/amenbreak/releases/download/audio2/amenbreak_data.tar.gz
+tar -xvzf amenbreak_data.tar.gz
+rm amenbreak_data.tar.gz

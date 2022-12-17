754feb0390a77f666518367753d53318a87710a6
Zack Scholl <zack.scholl@gmail.com>
2022-12-17 06:57:18 -0800

    use tar

--- lib/install.sh
+++ lib/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /home/we/dust/audio
-wget -q https://github.com/schollz/amenbreak/releases/download/audio2/amenbreak.tar.gz
-tar -xvzf amenbreak.tar.gz
-rm amenbreak.tar.gz
+wget -q https://github.com/schollz/amenbreak/releases/download/audio2/amenbreak.tar
+tar -xvf amenbreak.tar
+rm amenbreak.tar
 cd /home/we/dust/data
 wget -q https://github.com/schollz/amenbreak/releases/download/audio2/amenbreak_data.tar.gz
 tar -xvzf amenbreak_data.tar.gz
